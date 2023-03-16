8ea41cc531e40ad863fe62294802166467886793
ThurpatiNainesh <104067195+ThurpatiNainesh@users.noreply.github.com>
2023-03-16 09:00:48 +0530

    second

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express")
 const app = express()
 const mysql = require('mysql2');
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 const db = mysql.createConnection({
   host: "localhost",
   user:"root",
-  password:"nain@6013",
+  password:"nain@70113",
   database:"test",
   insecureAuth : true
 })
@@ -19,19 +18,20 @@ db.connect((err)=>{
     console.log("connected!!")
   }
 })
+
 app.get("/name",(req,res)=>{
   let {name}=req.query
-  let sql= `select * from books WHERE name = ${name} `
+  let sql= `SELECT * FROM books WHERE name = ${name} `
   db.execute(sql,function(err,result){
     if(err){
-       console.log(err)
+       return res.send("duplicate entry of name or id not allowed")
     }else{
-      res.status(200).json(result)
+      return res.send("user exist")
  }
 })
 })
 app.get("/sort",(req,res)=>{
-   let sql= "select * from books ORDER BY name ASC"
+   let sql= "SELECT * FROM books ORDER BY name ASC"
    db.execute(sql,function(err,result){
      if(err){
         console.log(err)
@@ -43,30 +43,25 @@ app.get("/sort",(req,res)=>{
 
 app.delete("/delete",(req,res)=>{
   let {name}=req.query
-  let sql= `DELETE from books WHERE name = ${name}`
+  let sql= `DELETE FROM books WHERE name = ${name}`
   db.execute(sql,function(err,result){
     if(err){
-       console.log(err)
+      return res.status(400).send("BOOK name does not exist in mysqlDB")
     }else{
       res.status(200).json(result)
  }
 })
 })
-app.post("/post", async(req,res)=>{
+app.post("/post",(req,res)=>{
   let data=req.body;
-//   let sql= `select * from books WHERE name = ${req.body.name} `
-//   db.execute(sql,function(err,result){
-//     if(err){
-//        console.log(err)
-//     }else{
-//      return  res.send("user alredy exist")
-//  }
-// })
-  db.query('insert into book SET ?',data,(err,result)=>{
+
+if(Object.keys(data) == 0) return res.status(400).send({ status: false, message: "please enter details" })
+ 
+     db.query('INSERT INTO book SET ?',data,(err,result)=>{
            if(err){
-             console.log(err)
+             return res.status(400).send("ID and name must be unique")
            }else{
-              res.status(201).json(result)
+              return res.status(201).json(result)
             }
   })
 })
@@ -74,3 +69,4 @@ app.post("/post", async(req,res)=>{
 app.listen(3000,()=>{
   console.log("server is running")
 })
+
